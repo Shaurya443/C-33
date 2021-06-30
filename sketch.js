14edd81f7d5dc0e77dace74a703e94efd6e7b2c2
Shaurya443 <81462790+Shaurya443@users.noreply.github.com>
2021-06-30 11:30:39 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,30 +9,33 @@ var snow1
 var world
 var penguinImg, penguin
 
+
 function preload(){
 bgImg = loadImage("bg.png")
-  penguinImg = loadAnimation("penguin1.png","penguin2.png")
+penguinImg = loadAnimation("penguin1.png","penguin2.png")
 }
 
 function setup() {
-  createCanvas(1000,700)
+  createCanvas(900,700)
   engine = Engine.create();
   world = engine.world;
 
+// Create snow
   snow1= new Snow(1050,200)
  snow1.scale = 0.9
-  
-  penguin = createSprite(900,700)
+
+ // create Sprite
+ penguin = createSprite(200,600,50,50)
  penguin.addAnimation("penguinWalk",penguinImg)
  penguin.scale = 1.0
  
-
    
 }
 function draw(){
 background(bgImg)
 Engine.update(engine);
 
+// snow display
 snow1.display();
  drawSprites();
 }
