710ac0fb3607b00fafa190d39d6c6f8a4c5d778b
Shaurya443 <81462790+Shaurya443@users.noreply.github.com>
2022-03-28 20:53:05 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,15 +17,17 @@ snow_Img = loadImage("snow1.png")
 }
 
 function setup() {
+  penguin = createSprite(200,600,50,50)
+ penguin.addAnimation("penguinWalk",penguinImg)
+ penguin.scale = 1.0
+ 
   createCanvas(900,700)
   engine = Engine.create();
   world = engine.world;
-  
-    snow1 = new Snow(200,50,50,50)
-  
- penguin = createSprite(200,600,50,50)
- penguin.addAnimation("penguinWalk",penguinImg)
- penguin.scale = 1.0
+
+
+
+ 
  
    
 }
@@ -33,15 +35,18 @@ function draw(){
 background(bgImg)
 Engine.update(engine);
 
-  snow1.display();
+if(frameCount % 60 === 0){
+  snow=createSprite(900,0,40,10)
+snow.x = Math.round(random(900,0));
+   snow.addImage("snowFalling",snow_Img)
+    snow.scale = 0.1;
+  snow.velocityY = 3
+    snow.lifetime=134
+  }
+
 
 
 drawSprites();
 
-}
-
-
-   
-  
 }
 
